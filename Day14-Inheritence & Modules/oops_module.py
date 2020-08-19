989ca50543e50bc3526b85c2280e3f8919628d70
madevi431 <51846949+madevi431@users.noreply.github.com>
2020-08-19 16:03:35 +0530

    Add files via upload

---
+++ Day14-Inheritence & Modules/oops_module.py
@@ -0,0 +1,16 @@
+class brach1():
+    def ece(self):
+        print("we are ece students")
+    def cse(self):
+        print("we ARE cse students")
+class barch2():
+    def mech(x):
+        print("we are mech students")
+    def civil(y):
+        print("we ARE civil students")
+class barch3():
+    def apssdc(z):
+        print("we are apssdc students")
+    def python(z):
+        print("we ARE python students")
+    
